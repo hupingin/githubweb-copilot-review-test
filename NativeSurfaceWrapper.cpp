c0b417bde9b9178ae21908553554c1180215133e
hopeking <34717499+hupingin@users.noreply.github.com>
2025-10-29 11:35:00 +0800

    Add files via upload

---
+++ NativeSurfaceWrapper.cpp
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ */
+
+#define LOG_TAG "NativeSurfaceWrapper"
+
+#include <android-base/properties.h>
+#include <gui/ISurfaceComposerClient.h>
+#include <gui/Surface.h>
+#include <gui/SurfaceComposerClient.h>
+#include <ui/DisplayState.h>
+#include <utils/Log.h>
+
+#include "NativeSurfaceWrapper.h"
+
+namespace android {
+
+NativeSurfaceWrapper::NativeSurfaceWrapper(const String8& name, uint32_t layerStack) 
+    : mName(name), mLayerStack(layerStack) {}
+
+void NativeSurfaceWrapper::onFirstRef() {
+    // create & init SurfaceComposerClient
+    sp<SurfaceComposerClient> surfaceComposerClient = new SurfaceComposerClient;
+    status_t err = surfaceComposerClient->initCheck();
+    if (err != OK) {
+        ALOGD("SurfaceComposerClient::initCheck error: %#x\n", err);
+        return;
+    }
+    // get ID for any displays
+    const std::vector<PhysicalDisplayId> ids = SurfaceComposerClient::getPhysicalDisplayIds();
+    if (ids.empty()) {
+        ALOGE("Failed to get ID for any displays\n");
+        return;
+    }
+    // get display information
+    sp<IBinder> displayToken = nullptr;
+    for (const auto id : ids) {
+        displayToken = SurfaceComposerClient::getPhysicalDisplayToken(id);
+        if (displayToken != nullptr) {
+            ui::DisplayState ds;
+            err = SurfaceComposerClient::getDisplayState(displayToken, &ds);
+            if(err == OK && ds.layerStack.id == mLayerStack) {
+                break;
+            }
+        }
+    }
+
+    // default -- display 0 is used
+    if (displayToken == nullptr) {
+        displayToken = SurfaceComposerClient::getPhysicalDisplayToken(ids.front());
+        mLayerStack = 0;
+        if (displayToken == nullptr) {
+            ALOGE("Failed to getPhysicalDisplayToken");
+            return;
+        }
+    }
+    // get display's resolution
+    ui::DisplayMode displayMode;
+    err = SurfaceComposerClient::getActiveDisplayMode(displayToken, &displayMode);
+    if (err != OK)
+        return;
+
+    ui::Size resolution = displayMode.resolution;
+    resolution = limitSurfaceSize(resolution.width, resolution.height);
+    // create the native surface
+    sp<SurfaceControl> surfaceControl = surfaceComposerClient->createSurface(mName, resolution.getWidth(), 
+                                                                             resolution.getHeight(), PIXEL_FORMAT_RGBA_8888,
+                                                                             ISurfaceComposerClient::eFXSurfaceBufferState,
+                                                                             /*parent*/ nullptr);
+
+    SurfaceComposerClient::Transaction{}
+            .setLayer(surfaceControl, std::numeric_limits<int32_t>::max())
+            .show(surfaceControl)
+            .setLayerStack(surfaceControl, ui::LayerStack::fromValue(mLayerStack))
+            .apply();
+
+    mWidth = resolution.getWidth();
+    mHeight = resolution.getHeight();
+    mSurfaceControl = surfaceControl;
+
+    ALOGI("onFirstRef: resolution: %dX%d\n", mWidth, mHeight);
+}
+
+sp<ANativeWindow> NativeSurfaceWrapper::getSurface() const {
+    sp<ANativeWindow> anw = mSurfaceControl->getSurface();
+    return anw;
+}
+
+ui::Size NativeSurfaceWrapper::limitSurfaceSize(int width, int height) const {
+    ui::Size limited(width, height);
+    bool wasLimited = false;
+    const float aspectRatio = float(width) / float(height);
+
+    int maxWidth = 600;
+    int maxHeight = 600;
+    // int maxWidth = android::base::GetIntProperty("ro.surface_flinger.max_graphics_width", 0);
+    // int maxHeight = android::base::GetIntProperty("ro.surface_flinger.max_graphics_height", 0);
+
+    if (maxWidth != 0 && width > maxWidth) {
+        limited.height = maxWidth / aspectRatio;
+        limited.width = maxWidth;
+        wasLimited = true;
+    }
+    if (maxHeight != 0 && limited.height > maxHeight) {
+        limited.height = maxHeight;
+        limited.width = maxHeight * aspectRatio;
+        wasLimited = true;
+    }
+    SLOGV_IF(wasLimited, "Surface size has been limited to [%dx%d] from [%dx%d]",
+             limited.width, limited.height, width, height);
+    return limited;
+}
+
+} // namespace android
---
+++ main_NativeSFDemo.cpp
@@ -0,0 +1,263 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ */
+
+#define LOG_TAG "NativeSFDemo"
+
+#include <binder/IPCThreadState.h>
+#include <binder/ProcessState.h>
+#include <binder/IServiceManager.h>
+#include <hardware/gralloc.h>
+#include <ui/GraphicBuffer.h>
+#include <utils/Log.h>
+
+#include "NativeSurfaceWrapper.h"
+
+using namespace android;
+
+bool mQuit = false;
+int mLayerStack = 0;
+
+void fillRGBA8Buffer(uint8_t* img, int width, int height, int stride, int r, int g, int b) {
+
+    ALOGI("fillRGBA8Buffer: resolution: %dX%d, stride:%d\n", width, height, stride);
+    
+    for (int y = 0; y < 720; y++) {
+        for (int x = 0; x < 1280; x++) {
+            uint8_t* pixel = img + (4 * (y*1280 + x));
+            pixel[0] = r;
+            pixel[1] = g;
+            pixel[2] = b;
+            pixel[3] = 0;
+        }
+    }
+
+    // for (int y = 0; y < height; y++) {
+    //     for (int x = 0; x < width; x++) {
+    //         uint8_t* pixel = img + (4 * (y*stride + x));
+    //         pixel[0] = r;
+    //         pixel[1] = g;
+    //         pixel[2] = b;
+    //         pixel[3] = 0;
+    //     }
+    // }
+}
+
+int drawNativeSurface(sp<NativeSurfaceWrapper> nativeSurface) {
+
+    ALOGI("drawNativeSurface 0\n");
+
+    status_t err = NO_ERROR;
+    int countFrame = 0;
+    ANativeWindowBuffer *nativeBuffer = nullptr;
+    ANativeWindow* nativeWindow = nativeSurface->getSurface().get();
+    // bool isFirstFrame = true;
+
+    ALOGI("drawNativeSurface 1\n");
+
+    // 1. connect the ANativeWindow as a CPU client. Buffers will be queued after being filled using the CPU
+    err = native_window_api_connect(nativeWindow, NATIVE_WINDOW_API_CPU);
+    if (err != NO_ERROR) {
+        ALOGE("ERROR: unable to native_window_api_connect\n");
+        return EXIT_FAILURE;
+    }
+
+    ALOGI("drawNativeSurface 2\n");
+    // 2. set the ANativeWindow dimensions
+    // err = native_window_set_buffers_user_dimensions(nativeWindow, 1200, 800);
+    err = native_window_set_buffers_user_dimensions(nativeWindow, nativeSurface->width(), nativeSurface->height());
+    if (err != NO_ERROR) {
+        ALOGE("ERROR: unable to native_window_set_buffers_user_dimensions\n");
+        return EXIT_FAILURE;
+    }
+ALOGI("drawNativeSurface 3\n");
+    // 3. set the ANativeWindow format
+    err = native_window_set_buffers_format(nativeWindow, PIXEL_FORMAT_RGBX_8888);
+    if (err != NO_ERROR) {
+        ALOGE("ERROR: unable to native_window_set_buffers_format\n");
+        return EXIT_FAILURE;
+    }
+    ALOGI("drawNativeSurface 4\n");
+
+    // 4. set the ANativeWindow usage
+    err = native_window_set_usage(nativeWindow, GRALLOC_USAGE_SW_WRITE_OFTEN);
+    if (err != NO_ERROR) {
+        ALOGE("native_window_set_usage failed: %s (%d)", strerror(-err), -err);
+        return err;
+    }
+
+    ALOGI("drawNativeSurface 5\n");
+    // 5. set the ANativeWindow scale mode
+    err = native_window_set_scaling_mode(nativeWindow, NATIVE_WINDOW_SCALING_MODE_SCALE_TO_WINDOW);
+    if (err != NO_ERROR) {
+        ALOGE("native_window_set_scaling_mode failed: %s (%d)", strerror(-err), -err);
+        return err;
+    }
+
+    ALOGI("drawNativeSurface 6\n");
+
+    // 6. set the ANativeWindow permission to allocte new buffer, default is true
+    static_cast<Surface*>(nativeWindow)->getIGraphicBufferProducer()->allowAllocation(true);
+
+    // 7. set the ANativeWindow buffer count
+    int numBufs = 0;
+    int minUndequeuedBufs = 0;
+
+    err = nativeWindow->query(nativeWindow,
+            NATIVE_WINDOW_MIN_UNDEQUEUED_BUFFERS, &minUndequeuedBufs);
+    if (err != NO_ERROR) {
+        ALOGE("error: MIN_UNDEQUEUED_BUFFERS query "
+                "failed: %s (%d)", strerror(-err), -err);
+        goto handle_error;
+    }
+
+    ALOGI("drawNativeSurface 7\n");
+
+    numBufs = minUndequeuedBufs + 1;
+    err = native_window_set_buffer_count(nativeWindow, numBufs);
+    if (err != NO_ERROR) {
+        ALOGE("error: set_buffer_count failed: %s (%d)", strerror(-err), -err);
+        goto handle_error;
+    }
+
+    ALOGI("drawNativeSurface 8\n");
+    
+    // 8. draw the ANativeWindow
+    while(!mQuit) {
+        // 9. dequeue a buffer
+        int releaseFenceFd = -1;
+        err = nativeWindow->dequeueBuffer(nativeWindow, &nativeBuffer, &releaseFenceFd);
+        if (err != NO_ERROR) {
+            ALOGE("error: dequeueBuffer failed: %s (%d)",
+                    strerror(-err), -err);
+            break;
+        }
+
+        ALOGI("drawNativeSurface 10\n");
+
+        // 10. make sure really control the dequeued buffer
+        sp<Fence> releaseFence(new Fence(releaseFenceFd));
+        int waitResult = releaseFence->waitForever("dequeueBuffer_EmptyNative");
+        if (waitResult != OK) {
+            ALOGE("dequeueBuffer_EmptyNative: Fence::wait returned an error: %d", waitResult);
+            break;
+        }
+
+        ALOGI("drawNativeSurface 11\n");
+
+        sp<GraphicBuffer> buf(GraphicBuffer::from(nativeBuffer));
+
+        // 11. call lock to get the virtual address of this buffer
+        uint8_t* img = nullptr;
+        err = buf->lock(GRALLOC_USAGE_SW_WRITE_OFTEN, (void**)(&img));
+        if (err != NO_ERROR) {
+            ALOGE("error: lock failed: %s (%d)", strerror(-err), -err);
+            break;
+        }
+
+        ALOGI("drawNativeSurface 12\n");
+
+        //12. fill buffer with RGB data
+        countFrame = (countFrame+1)%3;
+        /*if (isFirstFrame) {
+            isFirstFrame = false;
+            for (int y = 0; y < nativeSurface->height(); y++) {
+                for (int x = 0; x < nativeSurface->width(); x++) {
+                    uint8_t* pixel = img + (4 * (y*buf->getStride() + x));
+                    pixel[0] = 0;
+                    pixel[1] = 0;
+                    pixel[2] = 0;
+                    pixel[3] = 0;
+                }
+            }
+        } else {*/
+            fillRGBA8Buffer(img, nativeSurface->width(), nativeSurface->height(), buf->getStride(),
+                            countFrame == 0 ? 255 : 0,
+                            countFrame == 1 ? 255 : 0,
+                            countFrame == 2 ? 255 : 0);
+        // }
+
+        ALOGI("drawNativeSurface 13\n");
+
+        err = buf->unlock();
+        if (err != NO_ERROR) {
+            ALOGE("error: unlock failed: %s (%d)", strerror(-err), -err);
+            break;
+        }
+
+        ALOGI("drawNativeSurface 14\n");
+
+        // 13. queue the buffer to display
+        int acquireFenceFd = -1;
+        err = nativeWindow->queueBuffer(nativeWindow, buf->getNativeBuffer(), acquireFenceFd);
+        if (err != NO_ERROR) {
+            ALOGE("error: queueBuffer failed: %s (%d)", strerror(-err), -err);
+            break;
+        }
+
+        ALOGI("drawNativeSurface 15\n");
+
+        nativeBuffer = nullptr;
+        sleep(1);
+    }
+
+handle_error:
+    // 14. cancel buffer
+    if (nativeBuffer != nullptr) {
+        nativeWindow->cancelBuffer(nativeWindow, nativeBuffer, -1);
+        nativeBuffer = nullptr;
+    }
+
+    // 15. Clean up after success or error.
+    err = native_window_api_disconnect(nativeWindow, NATIVE_WINDOW_API_CPU);
+    if (err != NO_ERROR) {
+        ALOGE("error: api_disconnect failed: %s (%d)", strerror(-err), -err);
+    }
+
+    return err;
+}
+
+void sighandler(int num) {
+    if(num == SIGINT) {
+        printf("\nSIGINT: Force to stop !\n");
+        mQuit = true;
+    }
+}
+
+static void usage(const char *me)
+{
+    fprintf(stderr, "\nusage: \t%s [options]\n"
+                    "\t--------------------------------------- options ------------------------------------------------\n"
+                    "\t[-h] help\n"
+                    "\t[-d] layer stack(In the case of multi-display, NativeSFDemo shows on the specified displays \n"
+                    "\t                 in addition to the primary display)\n"
+                    "\t------------------------------------------------------------------------------------------------\n",
+                    me);
+    exit(1);
+}
+
+void parseOptions(int argc, char **argv) {
+    const char *me = argv[0];
+    int res;
+    while((res = getopt(argc, argv, "d:")) >= 0) {
+        switch(res) {
+            case 'd':
+                mLayerStack = atoi(optarg);
+                break;
+            case 'h':
+            default:
+            {
+                usage(me);
+            }
+        }
+    }
+}
+
+int main(int argc, char ** argv) {
+    parseOptions(argc, argv);
+    signal(SIGINT, sighandler);
+
+    sp<NativeSurfaceWrapper> nativeSurface(new NativeSurfaceWrapper(String8("NativeSFDemo"), mLayerStack));
+    drawNativeSurface(nativeSurface);
+    return 0;
+}
